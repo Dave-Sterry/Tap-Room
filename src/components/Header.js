b2149ac3ba66401f1cd282a5a1385fc067aad2e5
David Sterry <sterry.david@gmail.com>
2021-02-12 08:38:01 -0800

    add basic header and app files

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//find image to use for header!
+
+function Header(){
+  return(
+    <React.Fragment>
+      <h1>KegTown</h1>
+    </React.Fragment>
+  );
+}
+
+export default Header;
